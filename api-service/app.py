d66027590af8c975246cc8035271c26f5451e327
Jake21x <jakegarbo21@gmail.com>
2021-07-12 15:25:05 +0800

    api: volumn to copy created file from container to host

--- api-service/app.py
+++ api-service/app.py
@@ -1,21 +1,30 @@
 from flask import Flask
-from flask_restful import Resource, Api
-import json
+from flask_restful import Resource, Api 
 from flask import request
-from flask_httpauth import HTTPBasicAuth
-import jwt
-import os,datetime
+from flask_httpauth import HTTPBasicAuth 
+import jwt,json,os,datetime,time,random,base64
+from random import randint 
 from functools import wraps
+from itertools import chain
 
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+APP_ROOT_UPLOAD = os.path.dirname(os.path.abspath('api-service'))
+UPLOAD_FOLDER = 'uploads' 
 
 app = Flask(__name__)
 api = Api(app)
 auth = HTTPBasicAuth()
 app.config['SECRET_KEY'] = 'mykey'
 
-
 USER_DATA = {"admin":"admin"}
 
+def server_generated_id(): 
+    candidateChars = "0123456789abcdefghijklmnopqrstuvwxyz"
+    length = 7
+    sb = ""
+    for i in range(0, length):
+        sb = sb + str(candidateChars[random.randint(0, len(candidateChars)-1)])
+    return 'SR'+(str(datetime.datetime.now().strftime("%y%m%d%H%M%S%f"))+str(sb)).upper()
 
 @auth.verify_password
 def verify(username, password):
@@ -52,7 +61,7 @@ def verify_token(f):
             return f(*args, **kwargs)
         except Exception as e:
             print(e)
-            return {"Message":"Token is Missing or invalid"}
+            return {"Message":"Token is Missing or invalid" + str(e)}
     return decorator
 
 
@@ -61,10 +70,33 @@ class HelloWorld(Resource):
     def get(self):
         return json.dumps({"Messagesssssss ":"ok "})
 
+        
+class UPFILE(Resource):
+    @verify_token
+    def post(self):
+
+        json_dict = request.get_json(force=True, silent=True)
+        x = len(json_dict) 
+        photo_name = []
+        for i in chain(range(0, x)): 
+            use_id = server_generated_id() 
+            if json_dict[i]['photo'] !=".":
+                with open(os.path.join(UPLOAD_FOLDER, use_id + ".jpg"), "wb") as fh:
+                    photo_name.append(str('uploads/' + use_id + ".jpg"), )
+                    fh.write(base64.b64decode(json_dict[i]['photo']))
+            else:
+                print('no photo')
+
+        list = os.listdir(UPLOAD_FOLDER)
+        number_files = len(list) 
+
+        print('json_dict > ',json_dict , number_files)
+        return json.dumps({"FILE ":"ok "})
+
 
 api.add_resource(Login, '/login')
 api.add_resource(HelloWorld, '/verify')
-
+api.add_resource(UPFILE, '/upload')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
