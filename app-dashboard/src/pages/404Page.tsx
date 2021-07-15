4eb08c165c6f42eeed63fa7b76ed85e79f54442f
Jake21x <jakegarbo21@gmail.com>
2021-07-15 13:30:47 +0800

    api(sku,updates): initial working and wip

---
+++ app-dashboard/src/pages/404Page.tsx
@@ -0,0 +1,16 @@
+import { Box, Flex, Text } from "@chakra-ui/layout";
+import { useLocation } from "react-router";
+
+function Page404() {
+  const { pathname } = useLocation();
+  return (
+    <Flex justify="center" direction="column" alignItems="center">
+      <Text fontWeight="bold" fontSize="lg">
+        404
+      </Text>
+      Page not found {pathname}
+    </Flex>
+  );
+}
+
+export default Page404;
