5df5d05cd824e8ac6ebdd3542c299c56739a15cb
Jake21x <jakegarbo21@gmail.com>
2021-07-16 22:18:08 +0800

    api(logsmobile): fix error

--- api-service/api/logs_mobile.py
+++ api-service/api/logs_mobile.py
@@ -17,6 +17,7 @@ class ApiPostLogsMobile(Resource):
             data = []
             for i in chain(range(0, x)): 
                 gid = json_dict[i]['mgenerated_id'] 
+                print('gid',str(gid))
                 json_dict[i]['mgenerated_id'] = server_generated_id() if gid in ('.','') else gid
 
                 data.append((
