879b3ef8cd583267f77adef553857242edeccbeb
Jake21x <jakegarbo21@gmail.com>
2021-07-17 00:47:03 +0800

    api(sku): update key

--- api-service/api/skus.py
+++ api-service/api/skus.py
@@ -5,8 +5,20 @@ class ApiGetSKUs(Resource):
     def get(self):
         conn = Database() 
 
-        cursor = conn.execute("select skuid as tblskuid,refid as tblrefid,catid,sap_name as sku_name,case_barcode,product_barcode,gross_price_case,gross_price_piece,packs_or_bags,'.' as image_path from skus",result=True)
-        print('ApiGetSKUs > cursor',cursor)
+        cursor = conn.execute("""
+            select skuid as tblskuid,
+            refid as tblrefid,
+            catid,
+            sap_name as sku_name,
+            case_barcode,
+            product_barcode,
+            gross_price_case,
+            gross_price_piece,
+            packs_or_bags,
+            quantity_per_case,
+            '.' as image_path
+            from skus""",result=True)
+             
         data  = [dict(((cursor.description[i][0]), value) for i, value in enumerate(row)) for row in cursor.fetchall()]
         print('ApiGetSKUs > data',data)
         return data
