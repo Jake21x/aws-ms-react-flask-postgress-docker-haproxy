9bac4f630fefd0091108ee0b15d26819e193c976
Jake21x <jakegarbo21@gmail.com>
2021-07-17 19:39:04 +0800

    api: add more get

--- api-service/api/announcement.py
+++ api-service/api/announcement.py
@@ -34,13 +34,13 @@ class ApiGetAnnAll(Resource):
         conn = Database() 
         json_dict = request.get_json(force=True, silent=True)
         try:  
-             
+                # Wed, 14 Jul 2021 00:00:00 GMT
             data = conn.execute("""
                 SELECT id as tblannouncementsid, 
                 (select CONCAT(firstname,\' \',lastname) from users where userid = announcements.tbluserid)  AS name, 
                 (select userrole from users,users_role where users.roleid = users_role.roleid AND userid = announcements.tbluserid ) AS position, 
                 announcements,
-                to_char(announcements.date_posted,'Dy, dd Mon YYYY HH12:MI:SS') as date_posted 
+                to_char(announcements.date_posted,'Dy, dd Mon YYYY 00:00:00 GMT') as date_posted 
                 FROM announcements  where announcements.date_posted::date >= now()::date - INTERVAL \'3 DAY\' AND 
                 announcements.date_posted::date <= now()::date ORDER BY date_posted DESC""",result=True)
             announcements = [dict(((data.description[i][0]), value) for i, value in enumerate(row)) for row in data.fetchall() if row]
