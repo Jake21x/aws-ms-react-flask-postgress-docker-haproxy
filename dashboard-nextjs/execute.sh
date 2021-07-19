20f377bc2d2e54eaabb19167f633f61d28dc998f
Jake21x <jakegarbo21@gmail.com>
2021-07-19 19:01:51 +0800

    bash: improved

--- dashboard-nextjs/execute.sh
+++ dashboard-nextjs/execute.sh
@@ -2,23 +2,36 @@
 
 service=${PWD##*/}  
 
-echo "Execute command [ build | run | bash | clean ]"
-read input
+read -p "Available Command :
+1.build
+2.run
+3.bash
+4.clean
+5.images
+6.status 
+Execute Command >> " input 
 
-if [ $input == 'build' ]
+if [ $input == 1 ]
 then
 git pull
 sudo docker build -t $service .
 sudo docker rmi $(sudo docker images | grep "^<none" | awk '{print $3}') --force
-elif [ $input == 'bash' ]
+elif [ $input == 2 ]
 then
-sudo docker run -it $service bash
-elif [ $input == 'run' ]
+    sudo docker run -it $service bash
+elif [ $input == 3 ]
 then
-sudo docker run $service
-elif [ $input == 'clean' ]
+    sudo docker run $service
+elif [ $input == 4 ]
 then
-bash ../docker-clean.sh
+    sudo docker rm -f $(sudo docker ps -a -q)
+    sudo docker rmi $(sudo docker images | grep "^<none" | awk '{print $3}') --force
+elif [ $input == 5 ]
+then 
+    sudo docker images
+elif [ $input == 6 ]
+then 
+    sudo docker-compose ps -a
 elif [ $input == 'stop' ]
 then
 sudo docker stop $service
--- execute.sh
+++ execute.sh
@@ -1,25 +1,72 @@
+# #!/usr/bin/env bash
+
+# service=${PWD##*/}  
+
+# echo "Execute command [ up | down | clean | images | status ]"
+# read input
+
+# if [ $input == 'up' ]
+# then
+#     sudo docker-compose up
+# elif [ $input == 'down' ]
+# then
+#     sudo docker-compose down
+#     sudo docker rm -f $(sudo docker ps -a -q)
+#     sudo docker rmi $(sudo docker images | grep "^<none" | awk '{print $3}') --force
+# elif [ $input == 'clean' ]
+# then
+#     sudo docker rm -f $(sudo docker ps -a -q)
+#     sudo docker rmi $(sudo docker images | grep "^<none" | awk '{print $3}') --force
+# elif [ $input == 'images' ]
+# then 
+#     sudo docker images
+# elif [ $input == 'status' ]
+# then 
+#     sudo docker-compose ps -a
+# elif [ $input == 'stop' ]
+#     echo "No command"
+# then
+#     echo Sorry command for $input not found
+# else
+#     echo Sorry command for $input not found
+# fi
+
 #!/usr/bin/env bash
 
 service=${PWD##*/}  
 
-echo "Execute command [ up | down | clean ]"
-read input
+read -p "Available Command :
+1.up
+2.down
+3.clean
+4.images
+5.status 
+Execute Command >> " input 
 
-if [ $input == 'up' ]
+if [ $input == 1 ]
 then
-sudo docker-compose up
-elif [ $input == 'down' ]
+    sudo docker-compose up
+elif [ $input == 2 ]
 then
-sudo docker-compose down
-sudo docker rm -f $(sudo docker ps -a -q)
-sudo docker rmi $(sudo docker images | grep "^<none" | awk '{print $3}') --force
-elif [ $input == 'clean' ]
+    sudo docker-compose down
+    sudo docker rm -f $(sudo docker ps -a -q)
+    sudo docker rmi $(sudo docker images | grep "^<none" | awk '{print $3}') --force
+elif [ $input == 3 ]
 then
-sudo docker rm -f $(sudo docker ps -a -q)
-sudo docker rmi $(sudo docker images | grep "^<none" | awk '{print $3}') --force
+    sudo docker rm -f $(sudo docker ps -a -q)
+    sudo docker rmi $(sudo docker images | grep "^<none" | awk '{print $3}') --force
+elif [ $input == 4 ]
+then
+    sudo docker images
+elif [ $input == 5 ]
+then 
+    sudo docker-compose ps -a
+elif [ $input == 6 ]
+then 
+    echo Sorry command for $input not found
 elif [ $input == 'stop' ]
 then
-echo Sorry command for $input not found
+    sudo docker stop $service
 else
-echo Sorry command for $input not found
+    echo Sorry command for $input not found
 fi
